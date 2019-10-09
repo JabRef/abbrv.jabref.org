906fcce336960d8f2bc7a14e0ff25872dfe0195f
Julin S <48789920+ju-sh@users.noreply.github.com>
2019-10-09 14:41:56 +0530

    Improve columnJournalLists.py
     * Use slicing instead of `range()` in loop
     * Use snake case variable names
     * Convert comments to module docstring
     * Use fstrings

--- combineJournalLists.py
+++ combineJournalLists.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python
 
-# Python script for combining several journal abbreviation lists
-# and producing an alphabetically sorted list. If the same journal
-# names are repeated, only the version found last is retained.
-#
-# Usage: combineJournalLists.py outfile infile1 infile2 ...
+"""
+Python script for combining several journal abbreviation lists
+and producing an alphabetically sorted list. If the same journal
+names are repeated, only the version found last is retained.
+
+Usage: combineJournalLists.py out_file in_file1 in_file2 ...
+"""
 
 import sys
 
-outFile = sys.argv[1]
+out_file = sys.argv[1]
 journal_dict = {}
 
-for i in range(2,len(sys.argv)):
+for in_file in sys.argv[2:]:
     count = 0
-    f = open(sys.argv[i], "r")
+    f = open(in_file, "r")
     for line in f:
         if "=" in line and line[0] != "#":
             count = count+1
             parts = line.partition("=")
             journal_dict[parts[0].strip()] = line.strip()
     f.close()
-    print(sys.argv[i]+": "+str(count))
+    print(f"{in_file}: {count}")
 
-print("Combined key count: "+str(len(journal_dict)))
+print(f"Combined key count: {len(journal_dict)}")
 
-f = open(outFile, "w")
+f = open(out_file, "w")
 for key in sorted(journal_dict.keys()):
     f.write(journal_dict[key]+"\n")
 f.close()
