5e7926632a89786f5643d21a1b53931dcc81050f
David Méndez <hello@davidemdot.com>
2019-11-02 11:23:47 +0100

    Update MathSciNet script for an easier usage

--- update_mathscinet.py
+++ update_mathscinet.py
@@ -1,23 +1,21 @@
-import urllib.request
-import csv
+#!/usr/bin/env python3
 
-csv_file_path = 'mathscinet.csv'
-urllib.request.urlretrieve('https://mathscinet.ams.org/msnhtml/annser.csv', csv_file_path)
+import pandas as pd
 
-with open(csv_file_path, mode='r') as csv_file, \
-     open('journal_abbreviations_mathematics.txt', 'w') as abbreviations_file:
-    abbreviations_file = csv.writer(abbreviations_file, delimiter='=',
-                                    quotechar='"', quoting=csv.QUOTE_MINIMAL)
+file_in = "https://mathscinet.ams.org/msnhtml/annser.csv"
+file_out = "journals/journal_abbreviations_mathematics.csv"
 
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            # print(f'Column names are {", ".join(row)}')
-            line_count += 1
+# Get the first two fields of the last version of MathSciNet data file, without empty values
+df_new = pd.read_csv(file_in, usecols=[0, 1]).dropna()[["Full Title", "Abbrev"]]
 
-        abbreviations_file.writerow([row["Full Title"], row["Abbrev"]])
-        # print(f'\t{row["Full Title"]} abbreviated {row["Abbrev"]}')
-        line_count += 1
+# Get our last mathematics data file
+df_old = pd.read_csv(file_out, sep=";", header=None, names=["Full Title", "Abbrev"])
 
-    print(f'Processed {line_count} lines.')
+# Concatenate, remove duplicates and sort by journal name
+df = pd.concat([df_new, df_old], axis=0).drop_duplicates().sort_values(by=["Full Title"])
+
+# Remove values where journal name is equal to abbreviation
+df = df[df["Full Title"].str.lower() != df["Abbrev"].str.lower()]
+
+# Save the end file in the same path as the old one
+df.to_csv(file_out, sep=";", escapechar="\\", index=False, header=False)
