ab4faea09891ee439cc190318aef72dccb305078
Oliver Kopp <kopp.dev@gmail.com>
2023-01-02 15:33:23 +0100

    Fix escaping (#117)

--- scripts/update_mathscinet.py
+++ scripts/update_mathscinet.py
@@ -9,14 +9,13 @@ file_out = "journals/journal_abbreviations_mathematics.csv"
 df_new = pd.read_csv(file_in, usecols=[0, 1]).dropna()[["Full Title", "Abbrev"]]
 
 # Get our last mathematics data file
-df_old = pd.read_csv(file_out, sep=";", header=None, names=["Full Title", "Abbrev"])
+df_old = pd.read_csv(file_out, sep=";", escapechar="\\", header=None, names=["Full Title", "Abbrev"])
 
 # Concatenate, remove duplicates and sort by journal name
-df = pd.concat([df_new, df_old], axis=0).drop_duplicates().sort_values(by=["Full Title"])
+df = pd.concat([df_new, df_old], axis=0).drop_duplicates().sort_values(by=["Full Title", "Abbrev"])
 
 # Remove values where journal name is equal to abbreviation
 df = df[df["Full Title"].str.lower() != df["Abbrev"].str.lower()]
 
 # Save the end file in the same path as the old one
 df.to_csv(file_out, sep=";", escapechar="\\", index=False, header=False)
-
