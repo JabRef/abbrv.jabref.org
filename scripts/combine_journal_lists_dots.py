0273c517444a4e3a71ba4fd7e437ae5e2f8bbaf8
Northword <44738481+northword@users.noreply.github.com>
2023-09-02 20:19:17 +0800

    Fix lists - Migrate from semicolon to comma as separator (#139)

--- scripts/combine_journal_lists_dots.py
+++ scripts/combine_journal_lists_dots.py
@@ -8,49 +8,56 @@ names are repeated, only the version found last is retained.
 This version of the script specifically combines the lists following the ISO4
 standard WITH dots after abbreviated words.
 
-Usage: combine_journal_lists.py
+Usage: combine_journal_lists.py [output_file]
 Input: see list of files below
-Output: writes file 'journalList_dots.csv'
+Output: writes file 'journalList_dots.csv' (or specified output file)
 """
 
 import sys
+import pandas as pd
 
+# Define the list of CSV files
 import_order = [
-  'journals/journal_abbreviations_acs.csv',
-  'journals/journal_abbreviations_ams.csv',
-  'journals/journal_abbreviations_general.csv',
-  'journals/journal_abbreviations_geology_physics.csv',
-  'journals/journal_abbreviations_ieee.csv',
-  'journals/journal_abbreviations_lifescience.csv',
-  'journals/journal_abbreviations_mathematics.csv',
-  'journals/journal_abbreviations_mechanical.csv',
-  'journals/journal_abbreviations_meteorology.csv',
-  'journals/journal_abbreviations_sociology.csv',
-  'journals/journal_abbreviations_webofscience-dots.csv'
+    'journals/journal_abbreviations_acs.csv',
+    'journals/journal_abbreviations_ams.csv',
+    'journals/journal_abbreviations_general.csv',
+    'journals/journal_abbreviations_geology_physics.csv',
+    'journals/journal_abbreviations_ieee.csv',
+    'journals/journal_abbreviations_lifescience.csv',
+    'journals/journal_abbreviations_mathematics.csv',
+    'journals/journal_abbreviations_mechanical.csv',
+    'journals/journal_abbreviations_meteorology.csv',
+    'journals/journal_abbreviations_sociology.csv',
+    'journals/journal_abbreviations_webofscience-dots.csv'
 ]
 
-if len(sys.argv) == 1:
-    out_file = 'journalList_dots.csv'
-else:
-   out_file = sys.argv[1]
-print(f"Writing : {out_file}")
 
-journal_dict = {}
+def main(output_filename):
+    # Read and merge CSV files
+    # dfs = [pd.read_csv(file, header=None) for file in import_order]
+    dfs = []
+    for file in import_order:
+        df = pd.read_csv(file, header=None)
+        dfs.append(df)
+        print(f"{file}: {len(df)}")
+    merged_df = pd.concat(dfs, ignore_index=True)
 
-for in_file in import_order:
-    count = 0
-    f = open(in_file, "r")
-    for line in f:
-        if ";" in line and line[0] != "#":
-            count += 1
-            parts = line.partition(";")
-            journal_dict[parts[0].strip()] = line.strip()
-    f.close()
-    print(f"{in_file}: {count}")
+    # Drop duplicates based on the first column value and keep the last one obtained
+    merged_df.drop_duplicates(subset=[0], keep='last', inplace=True)
 
-print(f"Combined key count: {len(journal_dict)}")
+    # Sort alphabetically
+    sorted_df = merged_df.sort_values(by=[0])
 
-f = open(out_file, "w")
-for key in sorted(journal_dict.keys()):
-    f.write(journal_dict[key]+"\n")
-f.close()
+    # Save the result to the specified CSV file and ensure values are quoted
+    sorted_df.to_csv(output_filename, index=False, header=False, quoting=1)
+
+    print(f"Write {output_filename}, Combined key count: {len(merged_df)}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "journalList_dots.csv"
+
+    main(filename)
